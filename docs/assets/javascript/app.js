f30fc6f1cde180c378871c61871a1d623ab93a41
Andrew Zdunek <andrewzdunek.dev@gmail.com>
2021-10-29 13:16:53 +0100

    adding favicons

--- docs/assets/javascript/app.js
+++
@@ -1,6 +0,0 @@
-document.body.addEventListener("touchstart", function () {
-    var allVideos = document.querySelectorAll('video');
-    for (var i = 0; i < allVideos.length; i++) {
-        allVideos[i].play();
-    }
-},{ once: true });
